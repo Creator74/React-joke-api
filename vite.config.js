925f046c3128900604fc6742e4ac8aaf2286fefa
Creator74 <dhvanigupta2007@gmail.com>
2024-06-11 18:27:59 +0530

    change base url

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-joke-api/",
-  server: {
-    host: "127.0.0.1",
-    port: 5174,
-  },
+  base: "/React-joke-api/",
+  // server: {
+  //   host: "127.0.0.1",
+  //   port: 5174,
+  // },
 });
