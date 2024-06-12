5aecff1188e9187a998e438e181c17998c99ebba
Creator74 <dhvanigupta2007@gmail.com>
2024-06-12 13:42:08 +0530

    multiple api call

--- src/App.jsx
+++ src/App.jsx
@@ -50,11 +50,77 @@ function JokeUi() {
   );
 }
 
+function MyForm() {
+  const [numApiCalls, setNumApiCalls] = useState(0);
+  console.log(numApiCalls);
+  const [data, setData] = useState([]);
+  const [areCallsInProgress, setAreCallsInProgress] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchJokeData() {
+    setData([]);
+    setAreCallsInProgress(true);
+    var urls = [];
+    for (var i = 1; i <= numApiCalls; i++) {
+      urls.push("https://v2.jokeapi.dev/joke/Programming");
+    }
+    console.log(urls);
+    try {
+      const requests = urls.map((url) => axios.get(url));
+      const responses = await Promise.all(requests);
+      const results = responses.map((response) => response.data);
+      console.log(results);
+      setData(results);
+      setAreCallsInProgress(false);
+    } catch (err) {
+      setError(err);
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`The number you entered was: ${numApiCalls}`);
+    fetchJokeData();
+  };
+  console.log(data);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Number : 
+          <input
+            type="text"
+            value={numApiCalls}
+            onChange={(e) => setNumApiCalls(e.target.value)}
+            disabled={areCallsInProgress ? true : false}
+          />
+        </label>
+      </form>
+
+      <div>
+        {data.map((value, index) => (
+          <div key={index} className="joke-container">
+            {value.type === "single" ? (
+              value.joke
+            ) : (
+              <>
+                <div>{value.setup}</div>
+                <div>{value.delivery}</div>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default function MyApp() {
   return (
     <div>
       <h1>My First React Page</h1>
-      <JokeUi />
+      {/* <JokeUi /> */}
+      <MyForm />
     </div>
   );
 }
